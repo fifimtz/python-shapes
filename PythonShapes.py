69abae60d318608ac6d415ab10bd112434c2aaeb
fifimtz <emtz0027@outlook.com>
2017-07-06 14:31:42 -0500

    Add files via upload

---
+++ PythonShapes.py
@@ -0,0 +1,25 @@
+from turtle import *
+import math
+
+# Name your Turtle.
+t = Turtle()
+
+# Set Up your screen and starting position.
+setup(500,300)
+x_pos = 0
+y_pos = 0
+t.setposition(x_pos, y_pos)
+
+### Write your code below:
+pendown()
+
+forward(20)
+right(90)
+back(20)
+right(90)
+forward(20)
+left(90)
+forward(20)
+
+#exitonclick
+exitonclick()
